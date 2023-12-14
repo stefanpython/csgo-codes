f4f399551dbf5fe6e0952d76e0b9a77a24d3832c
stefanpython <stefanhtml@yahoo.com>
2023-12-14 16:20:19 +0200

    Search for boxes by input

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
 import "./Responsievness.css";
 import "./Dropdown.css";
 
 function App() {
-  // Handles button redirect
-  const handleButtonClick = (url) => {
-    window.location.href = url;
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchInput(event.target.value);
   };
 
-  // Dropdown button
-  function toggleDropdown() {
-    var dropdown = document.getElementById("gameDropdown");
-    dropdown.classList.toggle("show");
-  }
+  const filteredBoxes = (boxName) => {
+    const lowerCaseSearch = searchInput.toLowerCase();
+    return boxName.toLowerCase().includes(lowerCaseSearch);
+  };
 
-  // Close the dropdown if the user clicks outside of it
+  const toggleDropdown = () => {
+    const dropdown = document.getElementById("gameDropdown");
+    dropdown.classList.toggle("show");
+  };
+
   window.onclick = function (event) {
     if (!event.target.matches(".dropdown-btn")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
         if (openDropdown.classList.contains("show")) {
           openDropdown.classList.remove("show");
         }
@@ -43,15 +48,6 @@ function App() {
         </button>
       </div>
 
-      <div className="mid-container">
-        <hr className="left" />
-        <img className="star-icon" src="./star.png" alt="star" />
-        <div className="featured">
-          <h1>POPULAR</h1>
-        </div>
-        <hr className="right" />
-      </div>
-
       <div className="filter-search">
         <div className="searchbar-container">
           <input
@@ -60,6 +56,8 @@ function App() {
             name=""
             id=""
             placeholder="Type to filter"
+            value={searchInput}
+            onChange={handleInputChange}
           />
 
           <div className="dropdown">
@@ -75,138 +73,159 @@ function App() {
         </div>
       </div>
 
+      <div className="mid-container">
+        <hr className="left" />
+        <img className="star-icon" src="./star.png" alt="star" />
+        <div className="featured">
+          <h1>POPULAR</h1>
+        </div>
+        <hr className="right" />
+      </div>
+
       <div className="box-container">
-        <div className="box">
-          <div class="rectangle-container">
-            <div class="rectangle">
-              <p>$ 0.50</p>
+        {filteredBoxes("ROULETTE CRASH COINFLIP") && (
+          <div className="box">
+            <div class="rectangle-container">
+              <div class="rectangle">
+                <p>$ 0.50</p>
+              </div>
             </div>
-          </div>
 
-          <div className="right-title">
-            <p>CSGOEMPIRE</p>
-          </div>
+            <div className="right-title">
+              <p>CSGOEMPIRE</p>
+            </div>
 
-          <div className="first-rectangle">
-            <img className="empire-logo logo" src="./empire.svg" alt="empire" />
-          </div>
+            <div className="first-rectangle">
+              <img
+                className="empire-logo logo"
+                src="./empire.svg"
+                alt="empire"
+              />
+            </div>
 
-          <div className="small-rectangles">
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./roulette.png" alt="roulette" />
+            <div className="small-rectangles">
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./roulette.png" alt="roulette" />
+                </div>
+
+                <h1>ROULETTE</h1>
               </div>
 
-              <h1>ROULETTE</h1>
-            </div>
-
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./crash.png" alt="crash" />
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./crash.png" alt="crash" />
+                </div>
+                <h1>CRASH</h1>
               </div>
-              <h1>CRASH</h1>
-            </div>
 
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./coin.png" alt="coin" />
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./coin.png" alt="coin" />
+                </div>
+                <h1>COINFLIP</h1>
               </div>
-              <h1>COINFLIP</h1>
+            </div>
+
+            <div className="dotcode-btn">
+              <button>+ CSGODOTCODE</button>
             </div>
           </div>
+        )}
 
-          <div className="dotcode-btn">
-            <button>+ CSGODOTCODE</button>
-          </div>
-        </div>
-
-        <div className="box">
-          <div class="rectangle-container">
-            <div class="rectangle">
-              <p>$ 0.50</p>
-            </div>
-          </div>
-
-          <div className="right-title">
-            <p>CSGOROLL</p>
-          </div>
-          <div className="first-rectangle">
-            <img className="roll-logo logo" src="./roll.svg" alt="roll" />
-          </div>
-
-          <div className="small-rectangles">
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./roulette.png" alt="roulette" />
+        {filteredBoxes("ROULETTE CRASH COINFLIP") && (
+          <div className="box">
+            <div class="rectangle-container">
+              <div class="rectangle">
+                <p>$ 0.50</p>
               </div>
-              <h1>ROULETTE</h1>
             </div>
 
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./crash.png" alt="crash" />
+            <div className="right-title">
+              <p>CSGOROLL</p>
+            </div>
+            <div className="first-rectangle">
+              <img className="roll-logo logo" src="./roll.svg" alt="roll" />
+            </div>
+
+            <div className="small-rectangles">
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./roulette.png" alt="roulette" />
+                </div>
+                <h1>ROULETTE</h1>
               </div>
-              <h1>CRASH</h1>
-            </div>
 
-            <div className="small-box">
-              <div className="small-img">
-                <img src="./coin.png" alt="coin" />
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./crash.png" alt="crash" />
+                </div>
+                <h1>CRASH</h1>
               </div>
-              <h1>COINFLIP</h1>
+
+              <div className="small-box">
+                <div className="small-img">
+                  <img src="./coin.png" alt="coin" />
+                </div>
+                <h1>COINFLIP</h1>
+              </div>
+            </div>
+
+            <div className="dotcode-btn">
+              <button>+ CSGODOTCODE</button>
             </div>
           </div>
+        )}
 
-          <div className="dotcode-btn">
-            <button>+ CSGODOTCODE</button>
-          </div>
-        </div>
+        {filteredBoxes("") && (
+          <div className="box">
+            <div class="rectangle-container">
+              <div class="rectangle">
+                <p>$ 0.50</p>
+              </div>
+            </div>
 
-        <div className="box">
-          <div class="rectangle-container">
-            <div class="rectangle">
-              <p>$ 0.50</p>
+            <div className="right-title">
+              <p>HELLCASE</p>
+            </div>
+            <div className="first-rectangle">
+              <img
+                className="hell-logo logo"
+                src="./hellcase.svg"
+                alt="hellcase"
+              />
+            </div>
+
+            <div className="small-rectangles"></div>
+
+            <div className="dotcode-btn right-btn">
+              <button>+ CSGODOTCODE</button>
             </div>
           </div>
+        )}
 
-          <div className="right-title">
-            <p>HELLCASE</p>
-          </div>
-          <div className="first-rectangle">
-            <img
-              className="hell-logo logo"
-              src="./hellcase.svg"
-              alt="hellcase"
-            />
-          </div>
+        {filteredBoxes("") && (
+          <div className="box">
+            <div class="rectangle-container">
+              <div class="rectangle">
+                <p>$ 0.50</p>
+              </div>
+            </div>
 
-          <div className="small-rectangles"></div>
+            <div className="right-title">
+              <p>DADDYSKINS</p>
+            </div>
+            <div className="first-rectangle">
+              <img className="daddy-logo logo" src="./daddy3.png" alt="daddy" />
+            </div>
 
-          <div className="dotcode-btn right-btn">
-            <button>+ CSGODOTCODE</button>
-          </div>
-        </div>
+            <div className="small-rectangles"></div>
 
-        <div className="box">
-          <div class="rectangle-container">
-            <div class="rectangle">
-              <p>$ 0.50</p>
+            <div className="dotcode-btn right-btn">
+              <button>+ CSGODOTCODE</button>
             </div>
           </div>
-
-          <div className="right-title">
-            <p>DADDYSKINS</p>
-          </div>
-          <div className="first-rectangle">
-            <img className="daddy-logo logo" src="./daddy3.png" alt="daddy" />
-          </div>
-
-          <div className="small-rectangles"></div>
-
-          <div className="dotcode-btn right-btn">
-            <button>+ CSGODOTCODE</button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
