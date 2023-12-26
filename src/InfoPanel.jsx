06c36d9e4ad53fc8c65b6dd712555a0408bd928c
stefanpython <stefanhtml@yahoo.com>
2023-12-26 18:18:21 +0200

    Implement copying button

--- src/InfoPanel.jsx
+++ src/InfoPanel.jsx
@@ -1,9 +1,18 @@
 import "./InfoPanel.css";
+import CopyToClipboard from "react-copy-to-clipboard";
 
 const InfoPanel = () => {
+  // Handles button redirect
+  const handleButtonClick = (url) => {
+    window.location.href = url;
+  };
+
+  // Handles copying code button
+
   return (
     <div className="info-box">
       <div className="title-right">
+        {/* CHANGE PANEL NAME HERE INSIDE <p> <p/> tags - (this is the name on the right top-side of panel)*/}
         <p>CSGORoll</p>
       </div>
 
@@ -34,27 +43,47 @@ const InfoPanel = () => {
           </div>
 
           <div className="buttons-grid">
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/bitcoin.png" alt="" />
               <p>Bitcoin</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/ether.png" alt="" />
               <p>Ethereum</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/paypall.png" alt="" />
               <p>Paypal</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/bank.png" alt="" />
               <p>Bank</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/skins.png" alt="" />
               <p>Skins</p>
             </button>
@@ -67,27 +96,47 @@ const InfoPanel = () => {
           </div>
 
           <div className="buttons-grid">
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/bitcoin.png" alt="" />
               <p>Bitcoin</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/ether.png" alt="" />
               <p>Ethereum</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/paypall.png" alt="" />
               <p>Paypal</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/bank.png" alt="" />
               <p>Bank</p>
             </button>
 
-            <button>
+            <button
+              onClick={
+                () => handleButtonClick("ADD WEBSITE LINK HERE INSIDE QUOTES") // ADD WEBSITE LINK HERE eg: http://www.google.com - make sure it has http:// or https:// prefix
+              }
+            >
               <img className="" src="./buttons/skins.png" alt="" />
               <p>Skins</p>
             </button>
@@ -99,7 +148,11 @@ const InfoPanel = () => {
         <div className="left-box">
           <h3>Code</h3>
 
-          <button>Jofa</button>
+          {/* Implement copying code button */}
+          <CopyToClipboard text="Jofa" onCopy={() => alert("Copied!")}>
+            {/* Put in here between button tags what you want to be copied, eg here is Jofa, <button>HERE</button> */}
+            <button>Jofa</button>
+          </CopyToClipboard>
 
           <div className="clipboard">
             <p>Click to copy</p>
